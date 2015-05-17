0d772058fed3c3fd877c963eef3f1cce25bb1520
Maxim <maksimbakirov@MacBook-Pro-Maksim.local>
2015-05-17 19:35:19 +0600

    Update

--- Pacman/Assets/Scripts/PlayerController.cs
+++ Pacman/Assets/Scripts/PlayerController.cs
@@ -4,12 +4,22 @@ using System.Collections;
 public class PlayerController : Mover {
 	private int vertical = 0;
 	private int horizontal = 0;
-	private int x = 0;
+	private int x = -1;
 	private int y = 0;
 
 	protected override void Start () {
 		base.Start();
 		canMove = true;
+	}
+
+	void OnTriggerEnter2D(Collider2D other) {
+
+		if (other.tag == "Food") {
+
+		} else if (other.tag == "Energizer") {
+			
+		}
+		other.gameObject.SetActive(false);
 	}
 
 	void Update () {
