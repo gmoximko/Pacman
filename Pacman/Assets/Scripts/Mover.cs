5fd3a64ee8d78546fbba75b154bb0e6eb6a7dc79
Maxim <maksimbakirov@MacBook-Pro-Maksim.local>
2015-05-17 13:45:05 +0600

    Update

---
+++ Pacman/Assets/Scripts/Mover.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public abstract class Mover : MonoBehaviour {
+	private Rigidbody2D body;
+	private Collider2D coll;
+	public LayerMask mask;
+	public bool canMove;
+
+	protected virtual void Start () {
+		body = GetComponent<Rigidbody2D> ();
+		coll = GetComponent<Collider2D> ();
+	}
+
+	protected bool move(int x, int y) {
+		Vector2 start = (Vector2)transform.position;
+		Vector2 end = new Vector2 (x, y) + start;
+		RaycastHit2D hit;
+
+		coll.enabled = false;
+		hit = Physics2D.Linecast (start, end, mask);
+		coll.enabled = true;
+
+		if (hit.transform == null) {
+			StartCoroutine(smoothMove(end));
+			return true;
+		}
+		return false;
+	}
+
+	private IEnumerator smoothMove(Vector2 direction) {
+		float sqrDistance = ((Vector2)transform.position - direction).sqrMagnitude;
+
+		while (sqrDistance > float.Epsilon) {
+			Vector2 newPos = Vector2.MoveTowards(body.position, direction, Time.deltaTime);
+			body.MovePosition(newPos);
+			sqrDistance = ((Vector2)transform.position - direction).sqrMagnitude;
+			yield return null;
+		}
+	}
+}
---
+++ Pacman/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : Mover {
+	public float speed;
+	
+	protected override void Start () {
+		base.Start();
+	}
+
+	void Update () {
+		int vertical = (int)Input.GetAxisRaw("Vertical");
+		int horizontal = (int)Input.GetAxisRaw("Horizontal");
+
+		if (vertical != 0) {
+			horizontal = 0;
+		}
+
+		if (vertical != 0 || horizontal != 0) {
+			Debug.Log(move (horizontal, vertical));
+		}
+	}
+}
