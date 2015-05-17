43ff00679f72e7cb38de49787c2691efbcaafdee
Maxim <maksimbakirov@MacBook-Pro-Maksim.local>
2015-05-17 16:55:23 +0600

    Update

--- Pacman/Assets/Scripts/PlayerController.cs
+++ Pacman/Assets/Scripts/PlayerController.cs
@@ -2,22 +2,32 @@
 using System.Collections;
 
 public class PlayerController : Mover {
-	
+	private int vertical = 0;
+	private int horizontal = 0;
+
 	protected override void Start () {
 		base.Start();
 		canMove = true;
 	}
 
 	void Update () {
-		int vertical = (int)Input.GetAxisRaw("Vertical");
-		int horizontal = (int)Input.GetAxisRaw("Horizontal");
+
+		if ((int)Input.GetAxisRaw ("Vertical") != 0) {
+			vertical = (int)Input.GetAxisRaw("Vertical");
+			horizontal = 0;
+		}
+
+		if ((int)Input.GetAxisRaw ("Horizontal") != 0) {
+			horizontal = (int)Input.GetAxisRaw("Horizontal");
+			vertical = 0;
+		}
 
 		if (vertical != 0) {
 			horizontal = 0;
 		}
 
 		if ((vertical != 0 || horizontal != 0) && canMove) {
-			Debug.Log(move (horizontal, vertical));
+			move (horizontal, vertical);
 		}
 	}
 }
