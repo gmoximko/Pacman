d0f139fbb10f0a6b9330347edb0a5ccfe3e0f464
Maxim <maksimbakirov@MacBook-Pro-Maksim.local>
2015-05-17 12:17:09 +0600

    Update

---
+++ Pacman/Assets/PlayerController.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+	private Rigidbody2D body;
+	public float speed;
+	
+	void Start () {
+		body = GetComponent<Rigidbody2D> ();
+	}
+
+	void Update () {
+		float vertical = Input.GetAxisRaw("Vertical");
+		float horizontal = Input.GetAxisRaw("Horizontal");
+
+		if (vertical != 0.0f) {
+			horizontal = 0.0f;
+		}
+
+		body.MovePosition (body.position + new Vector2 (horizontal, vertical) * speed * Time.deltaTime);
+	}
+}
