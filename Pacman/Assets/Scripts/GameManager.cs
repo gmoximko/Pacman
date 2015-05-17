650953e05f26a2ffecf5056d096dd6523b37e9bb
Maxim <maksimbakirov@MacBook-Pro-Maksim.local>
2015-05-17 19:48:01 +0600

    Update

---
+++ Pacman/Assets/Scripts/GameManager.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameManager : MonoBehaviour {
+	private int rows = 28;
+	private int columns = 31;
+	public static GameManager gameManager = null;
+	
+	void Start () {
+
+		if (gameManager == null) {
+			gameManager = this;
+		} else if (gameManager != this) {
+			Destroy(gameObject);
+		}
+		DontDestroyOnLoad (gameObject);
+		initGame ();
+	}
+
+	private void initGame() {
+
+		for (int i = 0; i < rows; i++) {
+			for (int j = 0; j < columns; j++) {
+
+			}
+		}
+	}
+	
+	void Update () {
+		
+	}
+}
