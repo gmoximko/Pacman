ce8cf92a7946c412f7a9308f64d6f8b92ddc39ff
Maxim <maksimbakirov@MacBook-Pro-Maksim.local>
2015-05-31 11:03:07 +0600

    Update

--- Pacman/Assets/Scripts/Akabei.cs
+++ Pacman/Assets/Scripts/Akabei.cs
@@ -4,17 +4,21 @@ using System.Collections.Generic;
 public class Akabei : Mover {
 	private GameObject pacman;
 	private Vector2 prevPos;
-	private readonly Vector2[] directions = { new Vector2 (-1.0f, 0.0f), 
-										      new Vector2 ( 1.0f, 0.0f),
-											  new Vector2 ( 0.0f,-1.0f),
-											  new Vector2 ( 0.0f, 1.0f) };
+	private Vector2 currentPos;
+	private readonly Vector2[] fodbiddenUp = { new Vector2 (15.0f, 20.0f), 
+											   new Vector2 (12.0f, 20.0f),
+											   new Vector2 (12.0f,  8.0f),
+											   new Vector2 (15.0f,  8.0f) };
+	private readonly Vector2[] directions  = { new Vector2 (-1.0f, 0.0f), 
+										       new Vector2 ( 1.0f, 0.0f),
+											   new Vector2 ( 0.0f,-1.0f),
+											   new Vector2 ( 0.0f, 1.0f) };
 	
 	protected Vector2 target;
 	
 	protected override void Start() {
 		base.Start ();
 		pacman = GameObject.FindGameObjectWithTag("Player");
-		prevPos = new Vector2(14.0f, 18.0f);
 	}
 
 	protected virtual void setTarget() {
@@ -23,11 +27,12 @@ public class Akabei : Mover {
 
 	private void searchPath(int x, int y) {
 		Vector2 chooseDir = new Vector2(0.0f, 0.0f);
-		Vector2 currentPos = (Vector2)transform.position;
 		float minDistance = 500.0f;
+		currentPos = (Vector2)transform.position;
 
 		foreach (Vector2 temp in directions) {
-			if (Physics2D.Linecast(currentPos, currentPos + temp, mask).transform == null && currentPos + temp != prevPos) {
+			if (Physics2D.Linecast(currentPos, currentPos + temp, mask).transform == null 
+			    && pathFree(currentPos + temp)) {
 				float currentDistance = Vector2.Distance(currentPos + temp, target);
 
 				if (minDistance > currentDistance) {
@@ -38,6 +43,21 @@ public class Akabei : Mover {
 		}
 		prevPos = currentPos;
 		move ((int)chooseDir.x, (int)chooseDir.y);
+	}
+
+	private bool pathFree(Vector2 nextPos) {
+
+		if (nextPos == prevPos) {
+			return false;
+		}
+
+		foreach (Vector2 temp in fodbiddenUp) {
+			if (temp == nextPos 
+			    && currentPos - nextPos == new Vector2(0.0f, -1.0f)) {//идём наверх в запрещённых точках
+				return false;
+			}
+		}
+		return true;
 	}
 
 	void Update() {
