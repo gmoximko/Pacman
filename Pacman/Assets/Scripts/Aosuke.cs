1026fda178cb59c7c6b956c84bd352df43d819f1
Maxim <maksimbakirov@MacBook-Pro-Maksim.local>
2015-05-31 13:05:01 +0600

    Update

---
+++ Pacman/Assets/Scripts/Aosuke.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Aosuke : Akabei {
+
+	protected override void Start() {
+		base.Start ();
+	}
+
+	protected override void setTarget() {
+		
+	}
+}
---
+++ Pacman/Assets/Scripts/Otoboke.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Otoboke : Akabei {
+
+	protected override void Start() {
+		base.Start ();
+	}
+
+	protected override void setTarget() {
+		
+	}
+}
