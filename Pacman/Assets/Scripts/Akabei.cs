d2d1eef20f30ddca57786f0992b5a6f3f17b996d
Maxim <maksimbakirov@MacBook-Pro-Maksim.local>
2015-06-01 21:00:47 +0600

    Update

---
+++ Pacman/Assets/Scripts/Akabei.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+
+public class Akabei : Ghost {
+
+	protected override void Start() {
+		base.Start ();
+		scatterPoint = new Vector2 (27.0f, 34.0f);
+	}
+
+	protected override void Chase() {
+		target = (Vector2)pacman.transform.position;
+	}
+}
